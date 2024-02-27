01dcdab3a14b75ee47421c9538fc30ab697fed12
Sakan <sakan.nir@gmail.com>
2024-02-27 20:33:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,14 +15,15 @@ logger.add('yoasobi.log',
 
 
 def main() -> None:
-    db_dir = 'yoasobi.db'
-    logger.debug(f'{db_dir = }')
-
-    sqlite_db: Engine = sqldb.connect_sqlite_db(db_dir)
-    logger.debug(f'{sqlite_db = }')
-
-    logger.info('Checking if \'sql_query_exist\' exists')
     try:
+        db_dir = 'yoasobi.db'
+        logger.debug(f'{db_dir = }')
+
+        sqlite_db: Engine = sqldb.connect_sqlite_db(db_dir)
+        logger.debug(f'{sqlite_db = }')
+
+        logger.info('Checking if \'sql_query_exist\' exists')
+
         while True:
             option_result: str | None = ut.choose_table_options()
             logger.debug(f'{option_result = }')
@@ -36,34 +37,34 @@ def main() -> None:
             else:
                 logger.info(f'{sql_query_exist = }. Break the loop...')
                 break
+
+        urls: list[str] = ws.return_url_list()
+        for url in urls:
+            lyrics_list: list[str] = ws.scrap(url)
+            logger.debug(f'{lyrics_list = }')
+
+            song_name: str = ws.extract_song_name_from_lyrics_list(lyrics_list)
+            logger.debug(f'{song_name = }')
+
+            lyrics: str = ws.extract_lyrics_from_lyrics_list(lyrics_list)
+            logger.debug(f'{lyrics = }')
+
+            words: list[str] = ext.extract_words_from_lyrics(lyrics)
+            logger.debug(f'{words = }')
+
+            romanized_words: list[str] = ext.extract_romanji_from_words(words)
+            logger.debug(f'{romanized_words = }')
+
+            part_of_speech_list: list[str] = ext.extract_part_of_speech_from_words(words)
+            logger.debug(f'{part_of_speech_list = }')
+
+            sqldb.insert_data(words, romanized_words, part_of_speech_list, song_name, db_dir)
+
+            print(f'{len(words) = }')
+            print(f'{len(romanized_words) = }')
+            print(f'{len(part_of_speech_list) = }')
     except Exception as e:
         logger.error(f'Error: {e}')
-
-    urls: list[str] = ws.return_url_list()
-    for url in urls:
-        lyrics_list: list[str] = ws.scrap(url)
-        logger.debug(f'{lyrics_list = }')
-
-        song_name: str = ws.extract_song_name_from_lyrics_list(lyrics_list)
-        logger.debug(f'{song_name = }')
-
-        lyrics: str = ws.extract_lyrics_from_lyrics_list(lyrics_list)
-        logger.debug(f'{lyrics = }')
-
-        words: list[str] = ext.extract_words_from_lyrics(lyrics)
-        logger.debug(f'{words = }')
-
-        romanized_words: list[str] = ext.extract_romanji_from_words(words)
-        logger.debug(f'{romanized_words = }')
-
-        part_of_speech_list: list[str] = ext.extract_part_of_speech_from_words(words)
-        logger.debug(f'{part_of_speech_list = }')
-
-        sqldb.insert_data(words, romanized_words, part_of_speech_list, song_name, db_dir)
-
-        print(f'{len(words) = }')
-        print(f'{len(romanized_words) = }')
-        print(f'{len(part_of_speech_list) = }')
 
 
 if __name__ == '__main__':
