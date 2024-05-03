83aa1506698d7fff9182d6843eb387e0ddf97424
Sakan <sakan.nir@gmail.com>
2024-05-03 23:48:48 +0700

    Update v1.0.0

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from sqlalchemy import Engine
 from loguru import logger
 
-from yoasobi_project import extract as ext
-from yoasobi_project import sqlite_db as sqldb
-from yoasobi_project import utils as ut
-from yoasobi_project import web_scrap as ws
-
-# Prevent Loguru to show log message in terminal.
-logger.remove()
+import yoasobi_project
 
 logger.add('yoasobi.log',
            format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {name} | {module} | {function} | {line} | {message}",
@@ -19,16 +13,16 @@ def main() -> None:
         db_dir = 'yoasobi.db'
         logger.debug(f'{db_dir = }')
 
-        sqlite_db: Engine = sqldb.connect_sqlite_db(db_dir)
+        sqlite_db: Engine = yoasobi_project.connect_sqlite_db(db_dir)
         logger.debug(f'{sqlite_db = }')
 
         logger.info('Checking if \'sql_query_exist\' exists')
 
         while True:
-            option_result: str | None = ut.choose_table_options()
+            option_result: str | None = yoasobi_project.choose_table_options()
             logger.debug(f'{option_result = }')
 
-            sql_query_exist: bool | None = ut.check_table_sql_query(option_result, sqlite_db)
+            sql_query_exist: bool | None = yoasobi_project.check_table_sql_query(option_result, sqlite_db)
             logger.debug(f'{sql_query_exist = }')
 
             if sql_query_exist:
@@ -38,27 +32,27 @@ def main() -> None:
                 logger.info(f'{sql_query_exist = }. Break the loop...')
                 break
 
-        urls: list[str] = ws.return_url_list()
+        urls: list[str] = yoasobi_project.return_url_list()
         for url in urls:
-            lyrics_list: list[str] = ws.scrap(url)
+            lyrics_list: list[str] = yoasobi_project.scrap(url)
             logger.debug(f'{lyrics_list = }')
 
-            song_name: str = ws.extract_song_name_from_lyrics_list(lyrics_list)
+            song_name: str = yoasobi_project.extract_song_name_from_lyrics_list(lyrics_list)
             logger.debug(f'{song_name = }')
 
-            lyrics: str = ws.extract_lyrics_from_lyrics_list(lyrics_list)
+            lyrics: str = yoasobi_project.extract_lyrics_from_lyrics_list(lyrics_list)
             logger.debug(f'{lyrics = }')
 
-            words: list[str] = ext.extract_words_from_lyrics(lyrics)
+            words: list[str] = yoasobi_project.extract_words_from_lyrics(lyrics)
             logger.debug(f'{words = }')
 
-            romanized_words: list[str] = ext.extract_romanji_from_words(words)
+            romanized_words: list[str] = yoasobi_project.extract_romanji_from_words(words)
             logger.debug(f'{romanized_words = }')
 
-            part_of_speech_list: list[str] = ext.extract_part_of_speech_from_words(words)
+            part_of_speech_list: list[str] = yoasobi_project.extract_part_of_speech_from_words(words)
             logger.debug(f'{part_of_speech_list = }')
 
-            sqldb.insert_data(words, romanized_words, part_of_speech_list, song_name, db_dir)
+            yoasobi_project.insert_data(words, romanized_words, part_of_speech_list, song_name, db_dir)
 
             print(f'{len(words) = }')
             print(f'{len(romanized_words) = }')
---
+++ yoasobi_project/__init__.py
@@ -0,0 +1,4 @@
+from .extract import extract_words_from_lyrics, extract_romanji_from_words, extract_part_of_speech_from_words
+from .sqlite_db import connect_sqlite_db, insert_data
+from .utils import choose_table_options, check_table_sql_query
+from .web_scrap import return_url_list, scrap, extract_song_name_from_lyrics_list, extract_lyrics_from_lyrics_list
