a7ff9d004edb7262894ce34fb95d67b950e4e1a0
Sakan <sakanbeer@gmail.com>
2024-05-18 04:45:41 +0700

    Update v1.2.0 - Changed from Thread to ThreadPoolExecutor for thread_fetch_page_source function in web_scrap.py - Set disable image loading and headless option for a Chrome web driver

--- yoasobi_project/web_scrap.py
+++ yoasobi_project/web_scrap.py
@@ -105,6 +105,10 @@ def fetch_page_source(url: str) -> str:
     # Run Chrome in headless mode (without GUI) for better performance
     chrome_options.add_argument('--headless')
 
+    chrome_options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/94.0.4606.81 Safari/537.36")
+
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 
     logger.info('Open browser')
