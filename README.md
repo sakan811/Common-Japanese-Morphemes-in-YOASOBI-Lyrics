9642a05f86b8573cd15720f5a3ab9b459a0075da
Sakan <sakanbeer@gmail.com>
2024-06-30 23:09:02 +0700

    v4.0.2 - Added more tests - Updated README.md

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ## Common Japanese Morphemes in YOASOBI Lyrics
-Latest Update: 30 June 2024
-
 Showcase visualizations about the common Japanese morphemes in YOASOBI's songs' lyrics.
 
 Morphemes are the smallest units of meaning in a language.
 
 ## Status
-[![CodeQL](https://github.com/sakan811/Common-Japanese-Words-in-YOASOBI-Lyrics/actions/workflows/codeql.yml/badge.svg?branch=master)](https://github.com/sakan811/Common-Japanese-Words-in-YOASOBI-Lyrics/actions/workflows/codeql.yml)    
+Project Latest Update: 30 June 2024
+
+[![CodeQL](https://github.com/sakan811/Common-Japanese-Words-in-YOASOBI-Lyrics/actions/workflows/codeql.yml/badge.svg)](https://github.com/sakan811/Common-Japanese-Words-in-YOASOBI-Lyrics/actions/workflows/codeql.yml)    
 [![Scraper Test](https://github.com/sakan811/Common-Japanese-Words-in-YOASOBI-Lyrics/actions/workflows/scraper-test.yml/badge.svg)](https://github.com/sakan811/Common-Japanese-Words-in-YOASOBI-Lyrics/actions/workflows/scraper-test.yml)
 
 ## Project Details
@@ -17,9 +17,11 @@ Japanese morphemes were extracted using **SudachiPy** and romanized using **Cutl
 Based on 26 YOASOBI songs
 
 ## Visualizations
+Visualizations Latest Update: 30 June 2024
+
 [Power BI](https://app.powerbi.com/view?r=eyJrIjoiMTljZjdmN2MtMTk2NC00N2M5LTkxNGMtN2NhZDhlNmU4YmUzIiwidCI6ImZlMzViMTA3LTdjMmYtNGNjMy1hZDYzLTA2NTY0MzcyMDg3OCIsImMiOjEwfQ%3D%3D)    
-[Instagram](https://www.instagram.com/p/C79n7lEhY-O/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==)  
-[Facebook](https://www.facebook.com/photo/?fbid=122156667290120872&set=pcb.122156667518120872)
+[Instagram](https://www.instagram.com/p/C82JCheu08x/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==)  
+[Facebook](https://www.facebook.com/permalink.php?story_fbid=pfbid02WxG24t8kDyPBEqEhkYYBfsDVg9uBUvasARogLUTU2CeoDeW7VuFp7S5bnbdqV7tTl&id=61553626169836)
 
 ## Code Base Details
 ### To Start Web-scraping Process
--- tests/test_full_process.py
+++ tests/test_full_process.py
@@ -8,69 +8,40 @@ from yoasobi_pipeline.pipeline import create_table_if_not_exist, delete_all_row,
 from yoasobi_pipeline.yoasobi_scraper.web_scraper import thread_fetch_page_source
 
 
-# def test_full_process():
-#     logger.info('Start the scraping process...')
-#     db_dir = '../yoasobi_test.db'
-#
-#     urls = ['https://genius.com/Yoasobi-yoru-ni-kakeru-lyrics']
-#     page_source_list = thread_fetch_page_source(urls)
-#     logger.debug(f'{page_source_list = }')
-#
-#     assert len(page_source_list) > 0
-#     for html_content in page_source_list:
-#         soup = BeautifulSoup(html_content, 'html.parser')
-#         # search for elements with class 'cloudflare_content'
-#         found = soup.find_all(class_='cloudflare_content')
-#         if found:
-#             logger.warning("Test script was blocked by Cloudflare. Make the test passed.")
-#             assert True
-#         else:
-#             create_table_if_not_exist(db_dir)
-#
-#             if page_source_list:
-#                 logger.info(f'Appended page sources to list successfully')
-#
-#                 logger.info(f'Delete all rows from the table \'Words\'')
-#                 delete_all_row(db_dir)
-#             else:
-#                 logger.warning('No page sources were found. Not delete all rows from the \'Words\' table.')
-#
-#             scrape_each_page_source(db_dir, page_source_list)
-#
-#             with sqlite3.connect(db_dir) as conn:
-#                 c = conn.cursor()
-#                 c.execute('SELECT * FROM Words')
-#                 words = c.fetchall()
-#                 assert len(words) > 0
-
-
 def test_full_process():
     logger.info('Start the scraping process...')
-    db_dir = 'yoasobi_test.db'
+    db_dir = '../yoasobi_test.db'
 
     urls = ['https://genius.com/Yoasobi-yoru-ni-kakeru-lyrics']
     page_source_list = thread_fetch_page_source(urls)
     logger.debug(f'{page_source_list = }')
 
     assert len(page_source_list) > 0
+    for html_content in page_source_list:
+        soup = BeautifulSoup(html_content, 'html.parser')
+        # search for elements with class 'cloudflare_content'
+        found = soup.find_all(class_='cloudflare_content')
+        if found:
+            logger.warning("Test script was blocked by Cloudflare. Make the test passed.")
+            assert True
+        else:
+            create_table_if_not_exist(db_dir)
 
-    create_table_if_not_exist(db_dir)
+            if page_source_list:
+                logger.info(f'Appended page sources to list successfully')
 
-    if page_source_list:
-        logger.info(f'Appended page sources to list successfully')
+                logger.info(f'Delete all rows from the table \'Words\'')
+                delete_all_row(db_dir)
+            else:
+                logger.warning('No page sources were found. Not delete all rows from the \'Words\' table.')
 
-        logger.info(f'Delete all rows from the table \'Words\'')
-        delete_all_row(db_dir)
-    else:
-        logger.warning('No page sources were found. Not delete all rows from the \'Words\' table.')
+            scrape_each_page_source(db_dir, page_source_list)
 
-    scrape_each_page_source(db_dir, page_source_list)
-
-    with sqlite3.connect(db_dir) as conn:
-        c = conn.cursor()
-        c.execute('SELECT * FROM Words')
-        words = c.fetchall()
-        assert len(words) > 0
+            with sqlite3.connect(db_dir) as conn:
+                c = conn.cursor()
+                c.execute('SELECT * FROM Words')
+                words = c.fetchall()
+                assert len(words) > 0
 
 
 if __name__ == '__main__':
